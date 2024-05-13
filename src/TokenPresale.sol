744afe1a98bb94ced5b173fc17518a63fcbeee6c
sudotx <64108848+sudotx@users.noreply.github.com>
2024-05-13 20:28:59 +0100

    cleanup

--- src/TokenPresale.sol
+++ src/TokenPresale.sol
@@ -34,31 +34,31 @@ interface ILaunchpad {
 
 contract TokenPresale is ILaunchpad {
     // Variables
-    address public creator;
+    address immutable creator;
 
-    uint256 public platformTax; // base 10000
-    uint256 public lockinPeriod; // lockin
+    uint256 public platformTax;
+    uint256 public lockinPeriod;
     uint256 public tokenPrice;
 
-    address public operator;
+    address immutable operator;
     string public name;
-    uint256 public immutable tokenUnit;
+    uint256 constant tokenUnit = 10 ** 18;
     address public immutable factory;
     uint256 public ethPricePerToken;
-    uint256 public tokenHardCap;
-    uint256 public minTokenBuy;
-    uint256 public maxTokenBuy;
-    uint256 public startDate;
-    uint256 public endDate;
+    uint256 constant public = 1000 ether;
+    uint256 immutable minTokenBuy;
+    uint256 immutable maxTokenBuy;
+    uint256 immutable startDate;
+    uint256 immutable endDate;
     uint256 public protocolFee;
-    address public protocolFeeAddress;
+    address immutable protocolFeeAddress;
     uint256 public releaseDelay;
     uint256 public vestingDuration;
     mapping(address => uint256) public purchasedAmount;
     mapping(address => uint256) public claimedAmount;
     uint256 public totalPurchasedAmount;
-    uint256 public wlBlockNumber;
-    uint256 public wlMinBalance;
+    uint256 immutable wlBlockNumber = block.number;
+    uint256 public wlMinBalance = 1 ether;
     uint256 public maxAllocation;
     uint256 public globalTaxRate;
     uint256 public whitelistTxRate;
@@ -83,13 +83,12 @@ contract TokenPresale is ILaunchpad {
 
     constructor(uint256 _protocolFee, address _protocolFeeAddress, address _operator, address _factory) {
         operator = _operator;
+        creator = msg.sender;
         protocolFee = _protocolFee;
         protocolFeeAddress = _protocolFeeAddress;
         factory = _factory;
-        tokenUnit = 10 ** 18;
         name = "Default Token Presale";
         ethPricePerToken = 0.1 ether;
-        tokenHardCap = 1000 ether;
         startDate = block.timestamp;
         endDate = startDate + 30 days;
         minTokenBuy = 1 ether;
@@ -97,8 +96,6 @@ contract TokenPresale is ILaunchpad {
         releaseDelay = 30 days;
         vestingDuration = 90 days;
         wlBlockNumber = block.number;
-        wlMinBalance = 1 ether;
-        wlRoot = 0x0;
     }
 
     /// Allows users to purchase tokens during the token presale.
@@ -107,7 +104,12 @@ contract TokenPresale is ILaunchpad {
     /// transfers the tokens to the buyer, and emits an event.
     /// @param proof The Merkle proof to verify the user is whitelisted.
 
-    function buyTokens(bytes32[] calldata proof) external payable {
+    function buyTokens(bytes32[] calldata proof, uint256 amount) external payable {
+        // check if kyc is enabled, if so check kyc of user
+
+        // check if user is blacklisted
+
+        // check if time is correct
         // buy tokens logic
         // Check if whitelisted
         // Check if enough ETH sent
@@ -207,9 +209,7 @@ contract TokenPresale is ILaunchpad {
     }
 
     /// Converts the provided Ether amount to the corresponding token amount.
-    ///
     /// This function calculates the token amount based on the current Ether price, transfers the Ether to the contract, transfers the tokens to the buyer, and emits an event.
-    ///
     /// @param ethAmount The amount of Ether to convert to tokens.
     /// @return num The calculated token amount.
     function ethToToken(uint256 ethAmount) public view returns (uint256 num) {
@@ -219,81 +219,60 @@ contract TokenPresale is ILaunchpad {
         // Emit event
     }
     /// Returns the amount of tokens purchased by the specified address.
-    ///
     /// This function checks if specified address exists in the purchasedAmount mapping, and if so, returns the amount of tokens purchased by that address. If the address is not in the mapping, it returns 0.
-    ///
     /// @param _address The address to check the purchased token amount for.
     function getPurchasedAmount(address _address) external view {
         // Check if address exists in mapping
-        // Return purchased amount
         // Return purchased amount
         // Check if address exists in mapping
         // Return 0 if address is not in mapping
         // Else return purchased amount
     }
     /// Returns the amount of tokens claimed by the specified address.
-    ///
     /// This function checks if the specified address exists in the claimedAmount mapping, and if so, returns the amount of tokens claimed by that address. If the address is not in the mapping, it returns 0.
-    ///
     /// @param _address The address to check the claimed token amount for.
     function getClaimedAmount(address _address) external view {
         // Check if address exists in mapping
         // Return claimed amount
     }
     /// Returns the total amount of tokens purchased across all addresses.
-    ///
     /// This function returns the total amount of tokens that have been purchased across all addresses participating in the token presale.
-    ///
     function getTotalPurchasedAmount() external view {
         // Return totalPurchasedAmount
     }
 
     /// Returns the protocol fee.
-    ///
     /// This function returns the current protocol fee that is charged for the token presale.
-    ///
     function getProtocolFee() external view {
         // Return protocolFee
     }
     /// Returns the protocol fee address.
-    ///
     /// This function returns the current protocol fee address that is used for the token presale.
-    ///
     function getProtocolFeeAddress() external view {
         // Return protocolFeeAddress
     }
     /// Returns the current operator address.
-    ///
     /// This function returns the address of the current operator for the token presale contract.
-    ///
     function getOperator() external view {
         // Return operator
     }
     /// Returns the factory address.
-    ///
     /// This function returns the address of the factory contract associated with this token presale contract.
-    ///
     function getFactory() external view {
         // Return factory
     }
     /// Returns the start date of the token presale.
-    ///
     /// This function returns the start date of the token presale.
-    ///
     function getStartDate() external view {
         // Return startDate
     }
     /// Returns the end date of the token presale.
-    ///
     /// This function returns the end date of the token presale.
-    ///
     function getEndDate() external view {
         // Return endDate
     }
     /// Returns the minimum token buy amount.
-    ///
     /// This function returns the current minimum token buy amount for the token presale.
-    ///
     function getMinTokenBuy() external view {
         // Return minTokenBuy
     }
@@ -301,30 +280,22 @@ contract TokenPresale is ILaunchpad {
         // Return maxTokenBuy
     }
     /// Returns the release delay.
-    ///
     /// This function returns the current release delay for the token presale.
-    ///
     function getReleaseDelay() external view {
         // Return releaseDelay
     }
     /// Returns the vesting duration.
-    ///
     /// This function returns the current vesting duration for the token presale.
-    ///
     function getVestingDuration() external view {
         // Return vestingDuration
     }
     /// Returns the whitelist block number.
-    ///
     /// This function returns the current whitelist block number for the token presale.
-    ///
     function getWLBlockNumber() external view {
         // Return wlBlockNumber
     }
     /// Returns the minimum whitelist balance.
-    ///
     /// This function returns the current minimum whitelist balance required for the token presale.
-    ///
     function getWLMinBalance() external view {
         // Return wlMinBalance
     }
@@ -349,42 +320,4 @@ contract TokenPresale is ILaunchpad {
         // and less than end date
         // and current block timestamp is greater than or equal to start of claim period
     }
-
-    // user whitelist allocation
-
-    // whitelist user (protoected)
-
-    // set allocation and tax (protected)
-
-    // set marketing wallet
-
-    // deposit: ie invest token into token sale
-    /// Allows a user to deposit a specified amount into the token presale.
-    ///
-    /// This function checks if KYC is enabled and the user has passed KYC, if the user is not blacklisted, and if the deposit is being made during the correct time period.
-    ///
-    /// @param amount The amount the user wants to deposit.
-    function deposit(uint256 amount) external {
-        // check if kyc is enabled, if so check kyc of user
-
-        // check if user is blacklisted
-
-        // check if time is correct
-    }
-
-    // calculate max allocation
-
-    // calculate max tax fre allocation
-
-    // get user tax rate
-
-    // claim usdc from private round
-
-    // check if user can claim
-
-    // send locked usd to admin wallet
-
-    // lock usd for vesting period
-    // calculate vested amount
-    // send vested amount to user
 }
