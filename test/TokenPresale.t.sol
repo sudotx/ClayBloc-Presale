0baa28360f64a9a4faf5be3b03395caf2f0fbccd
sudotx <64108848+sudotx@users.noreply.github.com>
2024-05-14 21:38:55 +0100

    rename test contract

--- test/TokenPresale.t.sol
+++ test/TokenPresale.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {TokenPresale} from "../src/TokenPresale.sol";
 
-contract CounterTest is Test {
+contract TokenPresaleTest is Test {
     TokenPresale public counter;
 
     function setUp() public {
