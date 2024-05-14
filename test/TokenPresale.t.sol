418a1fb84d57a2b026628a6e6a4693f0a6ccc4ea
sudotx <64108848+sudotx@users.noreply.github.com>
2024-05-14 21:36:29 +0100

    added to tests

--- test/TokenPresale.t.sol
+++ test/TokenPresale.t.sol
@@ -11,4 +11,37 @@ contract CounterTest is Test {
         counter = new TokenPresale(1, address(0), address(1), address(2));
         console2.log("address of counter", address(counter));
     }
+
+    // Methodology of Tests
+    // the main objectives of this token presale contract is to allow users
+    // participate
+
+    function TestBuyTokens() external {}
+    function TestBuyTokensFailUnsuccesfulTransfer() external {}
+    function TestBuyTokensFailReenter() external {}
+    function TestBuyTokensFailInvalidProof() external {}
+    function TestBuyTokensFailZeroValue() external {}
+    function TestBuyTokensFail() external {}
+    function TestBuyTokensFailInvalidState() external {}
+    function TestClaimTokensFailUserDepositedAndClaimed() external {}
+    function TestClaimTokensFailVestingPeriodStillActive() external {}
+    function TestClaimTokensInvalidCaller() external {}
+    function TestClaimTokensFailReenter() external {}
+    function TestClaimTokensFail() external {}
+    function TestWithdrawEth() external {}
+    function TestWithdrawEthFail() external {}
+    function TestWithdrawTokens() external {}
+    function TestWithdrawTokensFail() external {}
+    function TestEthToToken() external {}
+    function TestEthToTokenFail() external {}
+    function TestUpdateEthPricePerToken() external {}
+    function TestUpdateEthPricePerTokenFail() external {}
+    function TestIncreaseHardCap() external {}
+    function TestIncreaseHardCapFail() external {}
+    function TestTransferOperatorOwnership() external {}
+    function TestTransferOperatorOwnershipFail() external {}
+    function TestUpdateWhitelist() external {}
+    function TestUpdateWhitelistFail() external {}
+    function TestsetVestingDuration() external {}
+    function TestsetVestingDurationFail() external {}
 }
